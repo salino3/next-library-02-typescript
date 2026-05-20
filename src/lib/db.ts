17cd545fc471ebcaa26e2260c1ef777c0e56cc37
Salino3 <flaviobcn012@gmail.com>
2026-05-20 10:27:49 +0200

    connection db

---
+++ src/lib/db.ts
@@ -0,0 +1,9 @@
+import "server-only";
+import { Pool } from "pg";
+
+// Connection DB if I need new API call in addition to Python backend
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const query = (text: string, params?: any[]) => pool.query(text, params);
