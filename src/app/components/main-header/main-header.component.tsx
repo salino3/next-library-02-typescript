8b744cf0d00d965fccc5ac480150701312b34a6d
Salino3 <flaviobcn012@gmail.com>
2026-05-21 11:51:24 +0200

    refactoring links navigation application

--- src/app/components/main-header/main-header.component.tsx
+++ src/app/components/main-header/main-header.component.tsx
@@ -12,6 +12,11 @@ import "./main-header.styles.scss";
 //  const { pathname, query } = router;
 //  router.replace(pathname);
 
+interface LinkApp {
+  pathName: string;
+  title: string;
+}
+
 export const MainHeader = () => {
   const pathName = usePathname();
   console.log("clog2", pathName);
@@ -29,6 +34,22 @@ export const MainHeader = () => {
       .catch((err) => console.error("Error checking Next.js local API:", err));
   }, []);
 
+  // Links <nav>
+  const linksApp: LinkApp[] = [
+    {
+      pathName: "/",
+      title: "Dashboard",
+    },
+    {
+      pathName: "/books",
+      title: "Books",
+    },
+    {
+      pathName: "/authors",
+      title: "Authors",
+    },
+  ];
+
   return (
     <div className="rootMainHeader">
       <div className="containerMainHeader">
@@ -43,44 +64,21 @@ export const MainHeader = () => {
         <h3 className="title">Next App Library</h3>
         <nav>
           <ul>
-            <li>
-              <Link
-                style={
-                  {
-                    "--link-selected": pathName === "/" ? "underline" : "none",
-                  } as React.CSSProperties
-                }
-                href={"/"}
-              >
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link
-                style={
-                  {
-                    "--link-selected":
-                      pathName === "/books" ? "underline" : "none",
-                  } as React.CSSProperties
-                }
-                href={"/books"}
-              >
-                Books
-              </Link>
-            </li>
-            <li>
-              <Link
-                style={
-                  {
-                    "--link-selected":
-                      pathName === "/authors" ? "underline" : "none",
-                  } as React.CSSProperties
-                }
-                href={"/authors"}
-              >
-                Authors
-              </Link>
-            </li>
+            {linksApp.map((link) => (
+              <li key={link.pathName}>
+                <Link
+                  style={
+                    {
+                      "--link-selected":
+                        pathName === link.pathName ? "underline" : "none",
+                    } as React.CSSProperties
+                  }
+                  href={link.pathName}
+                >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
