dff289c1c9dbb1353d78f6fd248dc44fd0e23c85
Salino3 <flaviobcn012@gmail.com>
2026-05-21 17:26:41 +0200

    add header

--- src/app/(layouts)/books/page.tsx
+++ src/app/(layouts)/books/page.tsx
@@ -1,12 +1,17 @@
 "use client";
-
+import { Aside } from "@/app/components/aside/aside.component";
+import style from "../../page.module.scss";
 import "./books.styles.scss";
 
 const LayoutBooks = () => {
   return (
-    <div className="LayoutBooks">
-      <div className="booksContent">
-        <h1>Library with Next - LayoutBooks</h1>
+    <div className={style.rootHome}>
+      <Aside />
+      {/*  */}
+      <div className="LayoutBooks">
+        <div className="booksContent">
+          <h1>Library with Next - LayoutBooks</h1>
+        </div>
       </div>
     </div>
   );
