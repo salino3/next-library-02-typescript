8659ce1a17accaaed57901efc94620ae8fb91e1e
Salino3 <flaviobcn012@gmail.com>
2026-05-22 14:31:53 +0200

    add card title

--- src/app/(layouts)/(dashboard)/components/dashboard-content/dashboard-content.component.tsx
+++ src/app/(layouts)/(dashboard)/components/dashboard-content/dashboard-content.component.tsx
@@ -1,10 +1,17 @@
+"use client";
 import "./dashboard-content.styles.scss";
 
 export const DashboardContent = () => {
   return (
     <div className="DashboardContent">
-      <div className="boxTitle">
+      <div className="boxTitlePlusParagraph">
         <h1>Library with Next</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius eveniet
+          rem minus labore quia dolores et, temporibus fuga distinctio a,
+          possimus ab aliquid incidunt voluptatibus. Modi officia aliquam esse
+          corporis.
+        </p>
       </div>
     </div>
   );
