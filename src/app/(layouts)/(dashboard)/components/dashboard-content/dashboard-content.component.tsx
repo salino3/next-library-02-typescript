e2ecc5d51a3752bce130c87a425f1cfb9f03271d
Salino3 <flaviobcn012@gmail.com>
2026-05-22 10:35:43 +0200

    add background image

---
+++ src/app/(layouts)/(dashboard)/components/dashboard-content/dashboard-content.component.tsx
@@ -0,0 +1,11 @@
+import "./dashboard-content.styles.scss";
+
+export const DashboardContent = () => {
+  return (
+    <div className="DashboardContent">
+      <div className="boxTitle">
+        <h1>Library with Next</h1>
+      </div>
+    </div>
+  );
+};
