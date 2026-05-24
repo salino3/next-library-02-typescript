a3a7ca4654a5b2f91b9905226e86196adcd4d3c2
Salino3 <flaviobcn012@gmail.com>
2026-05-24 11:25:05 +0200

    add footer

--- src/app/(layouts)/(dashboard)/page.tsx
+++ src/app/(layouts)/(dashboard)/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { ServicesApp } from "@/app/service/service-app";
 import { DashboardContent } from "./components/dashboard-content/dashboard-content.component";
+import { Footer } from "@/app/components/footer/footer.component";
 import "./dashboard.styles.scss";
 
 export const LayoutDashboard = () => {
@@ -13,6 +14,7 @@ export const LayoutDashboard = () => {
   return (
     <div className="LayoutDashboard">
       <DashboardContent />
+      <Footer />
     </div>
   );
 };
---
+++ src/app/components/footer/footer.component.tsx
@@ -0,0 +1,35 @@
+import { ImageComponent } from "@/app/common-app/image/image.component";
+import "./footer.styles.scss";
+
+export const Footer = () => {
+  return (
+    <footer className="rootFooter">
+      <ImageComponent
+        src="/icons/Border_Footer.svg"
+        alt="icon"
+        lazy="lazy"
+        customStyle={"squareImgFooter"}
+        vertical
+      />
+      <div className="contanerFooter">
+        <ImageComponent
+          src="/icons/Mark_Icon.svg"
+          alt="icon"
+          lazy="lazy"
+          customStyle={"imgMarkIcon"}
+          vertical
+        />
+        <div className="footerTitle">
+          <h3>Next App Library</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+            provident harum! Similique voluptate repellendus, nesciunt accusamus
+            et aliquam delectus assumenda provident, modi quia adipisci vel,
+            ipsa neque optio in aspernatur.
+          </p>
+        </div>
+        <button>Lorem ipsum dolor sit</button>
+      </div>
+    </footer>
+  );
+};
