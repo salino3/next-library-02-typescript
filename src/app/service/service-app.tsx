9fe62b5371f66b309095eb9aaff6bd24b252b2bc
Salino3 <flaviobcn012@gmail.com>
2026-05-28 11:29:01 +0200

    add new serivce

--- src/app/service/service-app.tsx
+++ src/app/service/service-app.tsx
@@ -1,5 +1,6 @@
 import { CONSTANTS_SERVER } from "@/lib/constants-server";
 import axios, { AxiosResponse } from "axios";
+import { SearchBookResponse } from "./interface";
 
 export class ServicesApp {
   public static async getAllBooks(): Promise<AxiosResponse> {
@@ -7,5 +8,22 @@ export class ServicesApp {
       console.error(err);
       return Promise.reject(err);
     });
+  }
+
+  //
+  public static async getFilteredListBooks(
+    searchName: string = "",
+    offset: number = 0,
+  ): Promise<AxiosResponse<SearchBookResponse>> {
+    return await axios
+      .post(CONSTANTS_SERVER.URL_BACK + "books/search", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: searchName, limit: 10, offset }),
+      })
+      .catch((err) => {
+        console.error(err);
+        return Promise.reject(err);
+      });
   }
 }
