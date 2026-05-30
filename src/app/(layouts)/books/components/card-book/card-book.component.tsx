13074a5f625a3f30dc72761c01fd3740ccd71571
Salino3 <flaviobcn012@gmail.com>
2026-05-30 15:49:14 +0200

    show book data

--- src/app/(layouts)/books/components/card-book/card-book.component.tsx
+++ src/app/(layouts)/books/components/card-book/card-book.component.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import { Dispatch, SetStateAction } from "react";
 import { BookResponse } from "@/app/service/interface";
 import { ServicesApp } from "@/app/service/service-app";
 import "./card-book.styles.scss";
 
 interface CardBookProps {
   book: BookResponse;
+  setBookData: Dispatch<SetStateAction<Promise<BookResponse> | null>>;
 }
 
-export const CardBook = ({ book }: CardBookProps) => {
+export const CardBook = ({ book, setBookData }: CardBookProps) => {
   return (
-    <li key={book.id} className="rootCardBook">
+    <li className="rootCardBook">
       <h5 className="font-medium text-lg">{book.title}</h5>
-      <button onClick={() => ServicesApp.getBookInfo(String(book.id))}>
+      <button
+        onClick={() =>
+          setBookData(
+            ServicesApp.getBookInfo(String(book.id)) as Promise<BookResponse>,
+          )
+        }
+      >
         Show Book
       </button>
     </li>
--- src/app/(layouts)/books/components/founded-book/founded-book.component.tsx
+++ src/app/(layouts)/books/components/founded-book/founded-book.component.tsx
@@ -1,6 +1,28 @@
 "use client";
+import { use } from "react";
+import { BookResponse } from "@/app/service/interface";
 import "./founded-book.styles.scss";
 
-export const FoundedBook = () => {
-  return <div className="rootFoundedRoot">FoundedBook</div>;
+interface Props {
+  bookData: Promise<BookResponse> | null;
+}
+
+export const FoundedBook = ({ bookData }: Props) => {
+  if (!bookData) {
+    return <p>Select a book to view details.</p>;
+  }
+
+  const book = use(bookData);
+
+  if (!book) {
+    return <p>Book not found.</p>;
+  }
+
+  return (
+    <div className="rootFoundedRoot">
+      FoundedBook
+      {book?.id}
+      {book?.title}
+    </div>
+  );
 };
