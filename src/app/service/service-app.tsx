fff6dc4a259a77b3919a89846607d0ec6e5a092e
Salino3 <flaviobcn012@gmail.com>
2026-06-03 15:05:55 +0200

    add services for authors

--- src/app/service/service-app.tsx
+++ src/app/service/service-app.tsx
@@ -1,8 +1,16 @@
 import { CONSTANTS_SERVER } from "@/lib/constants-server";
 import axios, { AxiosResponse } from "axios";
-import { BookResponse, SearchBookResponse } from "./interface";
+import {
+  AuthorResponse,
+  BookResponse,
+  SearchAuthorResponse,
+  SearchBookResponse,
+} from "./interface";
 
 export class ServicesApp {
+  // Books
+
+  //
   public static async getAllBooks(): Promise<BookResponse[] | AxiosResponse> {
     return await axios.get(CONSTANTS_SERVER.URL_BACK + "books").catch((err) => {
       console.error(err);
@@ -40,6 +48,45 @@ export class ServicesApp {
   ): Promise<BookResponse | AxiosResponse> {
     return await axios
       .get(`${CONSTANTS_SERVER.URL_BACK}books/${bookId}`)
+      .then((res) => res.data)
+      .catch((err) => {
+        console.error(err);
+        return Promise.reject(err);
+      });
+  }
+
+  // Authors
+
+  //
+  public static async getFilteredListAuthors(
+    searchName: string = "",
+    offset: number = 0,
+  ): Promise<SearchAuthorResponse> {
+    return await axios
+      .post(
+        CONSTANTS_SERVER.URL_BACK + "authors/search",
+        {
+          name: searchName,
+          limit: 10,
+          offset: offset,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
+        },
+      )
+      .then((res) => res.data)
+      .catch((err) => {
+        console.error(err);
+        return Promise.reject(err);
+      });
+  }
+
+  //
+  public static async getAuthorInfo(
+    authorId: string,
+  ): Promise<AuthorResponse | AxiosResponse> {
+    return await axios
+      .get(`${CONSTANTS_SERVER.URL_BACK}authors/${authorId}`)
       .then((res) => res.data)
       .catch((err) => {
         console.error(err);
