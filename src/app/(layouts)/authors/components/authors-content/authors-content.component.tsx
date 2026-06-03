20c1c4fac3ce20118b1ac3c4d920bbf424ce12ac
Salino3 <flaviobcn012@gmail.com>
2026-06-03 14:48:07 +0200

    add first components authors route

---
+++ src/app/(layouts)/authors/components/authors-content/authors-content.component.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { Suspense, useEffect, useState } from "react";
+import { ServicesApp } from "@/app/service/service-app";
+
+import "./authors-content.styles.scss";
+
+export const AuthorsContent = () => {
+  return (
+    <div className="rootAuthorsContent">
+      <h1>Library with Next - Layout Authors</h1>
+      <div className="boxTextAuthors">
+        <div className="boxContentTextAuthors bCTA_01">
+          <h3 className="title">Title Books Content</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            libero, explicabo, temporibus eveniet ipsa quos ullam rerum quasi
+            ratione excepturi autem perspiciatis ducimus cupiditate. Commodi
+            voluptatum est quidem dicta! Cumque! Ipsam, ab accusamus? Rem,
+            perspiciatis. Nesciunt eius cum praesentium quos itaque adipisci, in
+            nihil recusandae ex at, explicabo quam, beatae dolor assumenda
+            provident tempore deserunt. Eos nesciunt cupiditate minus
+            doloremque!
+          </p>
+        </div>
+        <div className="boxContentTextAuthors bCTA_02">
+          <h3 className="title">Title Authors Content</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            libero, explicabo, temporibus eveniet ipsa quos ullam rerum quasi
+            ratione excepturi autem perspiciatis ducimus cupiditate. Commodi
+            voluptatum est quidem dicta! Cumque! Ipsam, ab accusamus? Rem,
+            perspiciatis. Nesciunt eius cum praesentium quos itaque adipisci, in
+            nihil recusandae ex at, explicabo quam, beatae dolor assumenda
+            provident tempore deserunt. Eos nesciunt cupiditate minus
+            doloremque!
+          </p>
+        </div>
+        <div className="boxContentTextAuthors bCTA_03">
+          <h3 className="title">Title Authors Content</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            libero, explicabo, temporibus eveniet ipsa quos ullam rerum quasi
+            ratione excepturi autem perspiciatis ducimus cupiditate. Commodi
+            voluptatum est quidem dicta! Cumque! Ipsam, ab accusamus? Rem,
+            perspiciatis. Nesciunt eius cum praesentium quos itaque adipisci, in
+            nihil recusandae ex at, explicabo quam, beatae dolor assumenda
+            provident tempore deserunt. Eos nesciunt cupiditate minus
+            doloremque!
+          </p>
+        </div>
+      </div>
+
+      {/* <FormSearchBook
+        setSearchTitle={setSearchTitle}
+        searchTitle={searchTitle}
+      />
+      <FoundedBook bookData={bookData} />
+
+      <Suspense fallback={<p>Loading book list...</p>}>
+        <ListBook dataPromise={dataBooksPromise} setBookData={setBookData} />
+      </Suspense> */}
+    </div>
+  );
+};
--- src/app/(layouts)/authors/page.tsx
+++ src/app/(layouts)/authors/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Aside } from "@/app/components/aside/aside.component";
 import { Footer } from "@/app/components/footer/footer.component";
+import { AuthorsContent } from "./components/authors-content/authors-content.component";
 import style from "../../page.module.scss";
 import "./authors.styles.scss";
 
@@ -10,9 +11,7 @@ const LayoutAuthors = () => {
       <Aside />
       {/*  */}
       <div className="LayoutAuthors">
-        <div className="booksContent">
-          <h1>Library with Next - LayoutAuthors</h1>
-        </div>
+        <AuthorsContent />
 
         <Footer />
       </div>
