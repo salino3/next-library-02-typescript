80e8d6d4141cfd29cb9febc862ece3fa9e594ce2
Salino3 <flaviobcn012@gmail.com>
2026-06-15 12:51:44 +0200

    fix

--- src/app/common-app/modal-app/modal-app.component.tsx
+++ src/app/common-app/modal-app/modal-app.component.tsx
@@ -15,7 +15,24 @@ export const ModalApp = ({ showModal, setShowModal, children }: Props) => {
     <div className="rootModalApp" onClick={() => setShowModal(false)}>
       <div className="containerModalApp" onClick={(e) => e.stopPropagation()}>
         <header>
-          <button onClick={() => setShowModal(false)}>Close</button>
+          <button
+            className="btnCloseModal"
+            onClick={() => setShowModal(false)}
+            aria-label="Close modal"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+          </button>{" "}
         </header>
         <div className="content">{children}</div>
       </div>
