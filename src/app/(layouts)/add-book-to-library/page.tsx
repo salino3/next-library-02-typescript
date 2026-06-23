e0332ce5ef795bf785d14f625fe1b1cba1547571
Salino3 <flaviobcn012@gmail.com>
2026-06-23 12:18:31 +0200

    add aside and footer

--- src/app/(layouts)/add-book-to-library/page.tsx
+++ src/app/(layouts)/add-book-to-library/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useState } from "react";
+import { Aside } from "@/app/components/aside/aside.component";
+import { Footer } from "@/app/components/footer/footer.component";
 import { AIAutofillForm } from "./components/ai-autofill-form";
 import { AddBookForm } from "./components/add-book-form";
 import { BookAutofillFormProps } from "@/app/service/interface";
+import style from "../../page.module.scss";
 import "./add-book-to-library.styles.scss";
 
 const initialState: BookAutofillFormProps = {
@@ -15,15 +18,18 @@ export default function BookAutofillForm() {
   const [formData, setFormData] = useState<BookAutofillFormProps>(initialState);
 
   return (
-    <div className="LayoutBookAutofillForm">
-      <div className="containerPage">
-        <h1>AI Book & Author Registry</h1>
+    <div className={style.rootHome}>
+      <Aside />
+      {/*  */}
+      <div className="LayoutBookAutofillForm">
+        <div className="containerPage">
+          <h1>AI Book & Author Registry</h1>
 
-        <AIAutofillForm setFormData={setFormData} />
+          <AIAutofillForm setFormData={setFormData} />
 
-        <hr />
-
-        <AddBookForm formData={formData} setFormData={setFormData} />
+          <AddBookForm formData={formData} setFormData={setFormData} />
+        </div>
+        <Footer />
       </div>
     </div>
   );
